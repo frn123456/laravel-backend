d218a6a78561871fcb4652aeb9d4b40d54b11739
francis <francisanino126@gmail.com>
2025-06-13 07:23:10 +0800

    changed cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://laravel-react-pearl.vercel.app/'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
 
